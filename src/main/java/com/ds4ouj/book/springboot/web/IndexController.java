1c1f9f4c9b96e1df6618ad1baa18d84dfcdbfa9a
jojaeng2 <ds4ouj@naver.com>
2021-11-08 16:41:31 +0900

    Login function

--- src/main/java/com/ds4ouj/book/springboot/web/IndexController.java
+++ src/main/java/com/ds4ouj/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.ds4ouj.book.springboot.web;
 
+import com.ds4ouj.book.springboot.config.auth.LoginUser;
+import com.ds4ouj.book.springboot.config.auth.dto.SessionUser;
 import com.ds4ouj.book.springboot.service.posts.PostsService;
 import com.ds4ouj.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -7,16 +9,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
